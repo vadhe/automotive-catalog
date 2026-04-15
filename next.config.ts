37be19f4a216ac029b6c0368487626bdab378ef7
vadhe <aldyvadhe@gmail.com>
2026-04-15 19:33:10 +0800

    chore: setup dependencies and configurations

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
